ee6e5ba45188f294c7d786be80a73a559eec9a13
Harris Enniss <henniss@google.com>
2024-03-01 16:32:36 -0800

    Add an option to limit the number of tests.

--- test.sh
+++ test.sh
@@ -10,12 +10,17 @@ script_root="$(dirname "$script_root")"
 cp sample-missions/* "$TEMP"
 chmod a+w "$TEMP"/*
 
+i=0
 for m in "$TEMP"/*.mission; do 
     echo "$m"
     ./core.sh "$m"
     echo ""
     echo "-------------"
     echo ""
+    i+=1
+    if (( i >= $MU_TEST_MAX )) ; then
+        break
+    fi
 done
 
 if ! [[ "$KEEP_TEST" == true ]] ; then
