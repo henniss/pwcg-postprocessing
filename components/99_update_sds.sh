53b41eaf813313f345fff4b0dca2a77e775bd89f
Harris Enniss <henniss@google.com>
2024-03-04 11:48:24 -0800

    Simplify update_sds, and add a check to skip missions not within the IL-2 directory.

--- components/99_update_sds.sh
+++ components/99_update_sds.sh
@@ -1,10 +1,11 @@
-
-sds2="/e/SteamLibrary/steamapps/common/IL-2 Sturmovik Battle of Stalingrad/data/Multiplayer/il2dserverCoopProxy.sds"
-
 apply () {
     mission_dir="$(dirname "${mission}")"
-    base="/e/SteamLibrary/steamapps/common/IL-2 Sturmovik Battle of Stalingrad/data/Multiplayer/"
+    base="${root}/data/Multiplayer/"
     rel="$(realpath --relative-to="${base}" "${mission}")"
+    
+    if (( 0 == $(find "$base" -wholename "$mission" | wc -l) )) ; then 
+        return 1
+    fi
 
     goal="$(cygpath -w "${rel%.*}")"
     goal="$(echo "$goal" | sed -e 's|\\|\\\\|')"
@@ -15,7 +16,7 @@ apply () {
     IP=$(ipconfig | grep "IPv4 Address" | cut -d ':' -f 2 | tr -d ' ' | grep -v "192\.168\..*")
     if [[ -n "$IP" ]]; then
       echo "$IP"
-      sed -i -re "s/ServerIP.*/ServerIP = \"$IP\"/" "$sds2"
+      sed -i -re "s/ServerIP.*/ServerIP = \"$IP\"/" "$sds"
     fi
 
     mkdir -p "${mission_dir}/trash"
