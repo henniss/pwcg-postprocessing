b1563a03ee47f378bebe5440ad3363fc78a76825
Harris Enniss <henniss@google.com>
2024-07-27 22:29:05 -0700

    Revise wind to also output to the briefing.

--- components/01_wind.sh
+++ components/01_wind.sh
@@ -4,7 +4,8 @@ shouldHalt=false
 
 
 apply () {
-    set -e
+    
+    tempfile=$(mktemp)
     
     AL="${PWCGInput}/${PWCGMAP}/AirfieldLocations.json"
     [[ -f "$AL" ]] || { echo "can't find '$AL'"; return 1 ; }
@@ -20,7 +21,7 @@ apply () {
     HEADING=$(printf "%.0f\n" $HEADING)
     echo "Adjusting wind heading for $HOMEBASE"
     echo "Take-Off Heading $HEADING"
-
+    
     if [[ $(( $RANDOM % 100 )) -lt 70 ]]; then
       echo "Selecting wind from narrow cone"
       base=$(($HEADING + 135))
@@ -35,9 +36,24 @@ apply () {
 /WindLayers/
 ka
 'a+2,'a+6s/:.*:/:    $new :/
+'a+2W $tempfile
+
 w
 q
 EOF
+
+
+    speed=$(cat $tempfile | sed -e 's/.*:.*:[[:space:]]*\([[:digit:]]*\);.*/\1/')
+    echo "${new@Q} ${speed@Q}"
+    dir=$((($new + 180) % 360))
+    briefing_text=$(perl -e "printf '<br><br>Wind: to %d at %d m\/s', $dir, $speed")
+    
+    echo "${briefing_text@Q}"
+    
+    echo "----"
+#    | 
+    cat "${subtitles}" | iconv -f "UTF-16LE" -t UTF-8 | sed -e "2s/\r$/$briefing_text\r/" | iconv -f "UTF-8" -t "UTF-16LE"  > "$tempfile"
+    mv "$tempfile" "$subtitles"
 
 # city_fire, city_firesmall, and villagesmoke all need to be manually turned.
 $ed "$mission" > /dev/null <<EOF
